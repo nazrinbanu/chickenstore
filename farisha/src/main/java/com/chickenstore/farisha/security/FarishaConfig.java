64725cba88ec16615e37e7f364929495a73e70d5
nazrinbanu <nazrin.shahul99@gmail.com>
2025-06-19 16:22:23 +0100

    fix:added some in security config file

--- farisha/src/main/java/com/chickenstore/farisha/security/FarishaConfig.java
+++ farisha/src/main/java/com/chickenstore/farisha/security/FarishaConfig.java
@@ -21,8 +21,13 @@ public class FarishaConfig {
 	public UserDetailsManager userDetailsManager(DataSource dataSource) {
 		
 		
-		      return new JdbcUserDetailsManager(dataSource);
+		     return new JdbcUserDetailsManager(dataSource);
 		      
+		      
+//	JdbcUserDetailsManager jdbcUserDetailsManager=new JdbcUserDetailsManager(dataSource);
+//    jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from user_details where user_id=?");		      
+//    jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");		      
+//    return jdbcUserDetailsManager; 
 	}
 	
 //Below code is for inmemory security
